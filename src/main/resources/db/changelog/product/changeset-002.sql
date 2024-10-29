44e50f8339ee8e6849fd2786fffa316cfd3dea5a
7iquid <taviefalcon@yahoo.com>
2024-10-29 09:10:49 +0800

    update how to use sql database liquibase migration

--- src/main/resources/db/changelog/product/changeset-002.sql
+++ src/main/resources/db/changelog/product/changeset-002.sql
@@ -1,25 +1,5 @@
-IF EXISTS (
-    SELECT 1
-    FROM information_schema.tables
-    WHERE
-        table_name = 'user'
-) THEN
--- Check if the 'role_user' table does not exist
-IF NOT EXISTS (
-    SELECT 1
-    FROM information_schema.tables
-    WHERE
-        table_name = 'role_user'
-) THEN
--- Create the 'role_user' table
-CREATE TABLE role_user (
-    id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
-    name VARCHAR(45),
-    role INT UNIQUE,
-    FOREIGN KEY (user_id) REFERENCES "user" (id) -- Quoted to handle reserved word
-);
+--liquibase formatted sql
+--changeset techgeeknext:create-tables
 
-END IF;
-
-END IF;
+ALTER TABLE `user` 
+    ADD COLUMN  age int;
