174dc044f2b99e263cbc723dbfc6b9f130701d43
7iquid <taviefalcon@yahoo.com>
2024-11-26 20:01:19 +0800

    update test

--- springcloud-mongo/src/test/java/com/mongo/MongoApplication.java
+++ springcloud-mongo/src/test/java/com/mongo/MongoApplication.java
@@ -1,3 +1,12 @@
+package com.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MongoApplication {
-    
+
+    @Test
+    void contextLoads() {
+    }
 }
