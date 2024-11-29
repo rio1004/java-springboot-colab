82c85d53e0ef2c9134eaa34632c6629a20f96868
tavs <82376053+7iquid@users.noreply.github.com>
2024-11-29 10:12:02 +0800

    working mongo

--- springcloud-mongo/src/main/java/com/mongo/model/Payment.java
+++ springcloud-mongo/src/main/java/com/mongo/model/Payment.java
@@ -3,15 +3,16 @@ package com.mongo.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.index.Indexed;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "payments")
 public class Payment {
 
